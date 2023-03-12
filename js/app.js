9561b86ef47d8da7616391bf0d6af286366cdb48
jib2004 <obasaajibola04@gmail.com>
2023-03-12 18:57:57 +0100

    js++

--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
-// //const sub_nav = document.querySelector('.special_li');
-// const special_li = document.querySelector('.special_li')
-// const dropdown_menu = document.querySelector('.drop')
-// const show = document.querySelector(".show")
+//const sub_nav = document.querySelector('.special_li');
+const special_li = document.querySelector('.special_li')
+const dropdown_menu = document.querySelector('.drop')
+const show = document.querySelector(".show")
 
 
-// special_li.addEventListener('mouseover',()=>{
-//     dropdown_menu.classList.add("show")
+special_li.addEventListener('click',(e)=>{
+    e.preventDefault;
+    dropdown_menu.classList.toggle("show")
     
-// })
+})
 
-// special_li.addEventListener('click',(e)=>{
-//     e.preventDefault;
-//     dropdown_menu.classList.toggle("show")
-    
-// })
-
-// window.addEventListener("click",()=>{
-//     dropdown_menu.classList.remove("show")
-// })
+window.addEventListener("click",()=>{
+    dropdown_menu.classList.remove("show")
+})
